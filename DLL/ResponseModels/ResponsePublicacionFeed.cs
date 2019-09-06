873659a0976314f2104016c8af418834a0974e51
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2019-09-06 11:34:32 -0600

    ResponseModels las respuestas del api se recibiran aca

---
+++ DLL/ResponseModels/ResponsePublicacionFeed.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace DLL.ResponseModels
+{
+    using BrotApi0.Models;
+    using System;
+    using System.Collections.Generic;
+    public class ResponsePublicacionFeed
+    {
+        public int id_post { get; set; }
+        public Nullable<int> id_user { get; set; }
+        public string img { get; set; }
+        public bool? isimg { get;set; }
+
+        public string descripcion { get; set; }
+        public Nullable<System.DateTime> fecha_creacion { get; set; }
+        public Nullable<System.DateTime> fecha_actualizacion { get; set; }
+
+        public users UsuarioCreator { get; set; }
+
+        public ICollection<comentarios> LComentarios { get; set; }
+        public ICollection<like_post> LLikesPost { get; set; }
+
+
+    }
+}
