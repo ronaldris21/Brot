5667452b29b095bb1923ba5fd5dc23690e881483
Vanegas9090 <mario.vanegas9090@gmail.com>
2019-09-10 11:32:05 -0600

    Custom Pin icon

--- BrotCliente/BrotCliente/ViewModels/PostViewModel.cs
+++ BrotCliente/BrotCliente/ViewModels/PostViewModel.cs
@@ -1,13 +1,46 @@
 ﻿using DLL.ResponseModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace BrotCliente.ViewModels
 {
     public class PostViewModel : BaseViewModel
     {
+        public ObservableCollection<string> lista { get; }
         private ResponsePublicacionFeed _Post;
+
+        public PostViewModel()
+        {
+            this.lista = new ObservableCollection<string>();
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+            this.lista.Add("asdfaksjdfhasjdkfhajdsfhakdjhfajksdfhajdhfjkasdhfjaksdhf");
+        }
 
         public ResponsePublicacionFeed Post
         {
