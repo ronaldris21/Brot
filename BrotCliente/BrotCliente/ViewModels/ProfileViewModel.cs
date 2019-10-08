a0781c544b4ce310326d61f7ad6ab84e2b5fe608
Vanegas9090 <mario.vanegas9090@gmail.com>
2019-10-08 09:52:10 -0600

    Preparing to pull changes

---
+++ BrotCliente/BrotCliente/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BrotCliente.Views;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BrotCliente.ViewModels
+{
+    public class ProfileViewModel : BaseViewModel
+    {
+        #region Attributes
+
+        public ICommand EditProfileCommand
+        {
+            get { return new RelayCommand(EditProfile); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void EditProfile()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new EditProfile());
+        }
+
+        #endregion
+    }
+}
---
+++ BrotCliente/BrotCliente/Views/Tabs/Profile.xaml.cs
@@ -0,0 +1,26 @@
+﻿using BrotCliente.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BrotCliente.Views.Tabs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(true)]
+    public partial class Profile : ContentPage
+    {
+        private ProfileViewModel ViewModel;
+        public Profile()
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = new ProfileViewModel();
+        }
+    }
+}
