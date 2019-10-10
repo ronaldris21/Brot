57b1589b47d8ee0db278083d1df49bdfbf4d97a3
Vanegas9090 <mario.vanegas9090@gmail.com>
2019-10-10 10:48:42 -0600

    asdf

--- BrotCliente/BrotCliente/Views/Tabs/Feed.xaml.cs
+++ BrotCliente/BrotCliente/Views/Tabs/Feed.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using BrotCliente.ViewModels;
 using DLL.ResponseModels;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
