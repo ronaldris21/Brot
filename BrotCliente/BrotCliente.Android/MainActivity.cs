f576f2b6f2da6c5cab5a54e9cbe5103009e15147
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2019-12-06 14:46:17 -0600

    Splash Screens

--- BrotCliente/BrotCliente.Android/MainActivity.cs
+++ BrotCliente/BrotCliente.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Acr.UserDialogs;
 
 namespace BrotCliente.Droid
 {
-    [Activity(Label = "BrotCliente", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Locked)]
+    [Activity(Label = "BrotCliente", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Locked)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
